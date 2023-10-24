e125f59fad345e1cad9de4ff2b7f58cdb77233fc
Pablo <andresbocel0@gmail.com>
2023-10-24 07:47:29 -0600

    1

--- PAT_Parcial_01_A/Ejercicio02.cpp
+++ PAT_Parcial_01_A/Ejercicio02.cpp
@@ -4,17 +4,21 @@ int Ejercicio02::countStudents(vector<char>* students, vector<char>* sandwiches)
 {
 	while (!students->empty() && !sandwiches->empty())
 	{
-		if (students[0] == sandwiches[0])
+		for (int i = 0; i < students->size(); i++)
 		{
-			students->erase(students->begin());
-			sandwiches->erase(sandwiches->begin());
-		}
-		else
-		{
-			students->push_back((*students)[0]); 
-			students->erase(students->begin()); 
+			if (students[i] == sandwiches[i])
+			{
+				students->erase(students->begin());
+				sandwiches->erase(sandwiches->begin());
+			}
+			else
+			{
+				students->push_back((*students)[i]);
+				students->erase(students->begin());
 
+			}
 		}
+		
 	}
 	return students->size(); 
 }
